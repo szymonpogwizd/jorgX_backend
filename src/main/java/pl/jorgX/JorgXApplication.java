9731f5d49d6a3c726a22760c0b234b09aba4afa4
szymonpogwizd <szymonpogwizd12@gmail.com>
2024-03-27 18:29:10 +0100

    JORGX-42 - Initial Project Setup

---
+++ src/main/java/pl/jorgX/JorgXApplication.java
@@ -0,0 +1,13 @@
+package pl.jorgX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JorgXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JorgXApplication.class, args);
+    }
+
+}
