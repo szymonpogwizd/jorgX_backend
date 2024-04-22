4041d56368bd8cb17740d744cd259c161f6b9cdc
Szymon Pogwizd <116025180+szymonpogwizd@users.noreply.github.com>
2024-04-22 19:49:15 +0200

    JORGX-79 - Fix endpoints (#8)

--- src/main/java/pl/jorgX/database/opinion/OpinionRepository.java
+++ src/main/java/pl/jorgX/database/opinion/OpinionRepository.java
@@ -2,10 +2,12 @@ package pl.jorgX.database.opinion;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface OpinionRepository extends JpaRepository<OpinionDAO, UUID> {
     List<OpinionDAO> findByPlaceId(UUID placeId);
 
