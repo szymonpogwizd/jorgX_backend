55d7841347d5092be5aa68403a30c084a21f9677
szymonpogwizd <szymonpogwizd12@gmail.com>
2024-05-17 09:56:30 +0200

    JORGX-93 - Add endpoint to fetch current user details

--- src/main/java/pl/jorgX/JorgXApplication.java
+++ src/main/java/pl/jorgX/JorgXApplication.java
@@ -11,12 +11,14 @@ public class JorgXApplication {
     }
 }
 
-// TODO do usunięcia po zakończeniu implementacji modelu - kod dla testów
-//public class JorgXApplication {
-//
-//    public static void main(String[] args) {
-//        ModelAPIConnector modelClient = new ModelAPIConnector("http://localhost:5000/predict");
-//        String result = modelClient.makeRequest("Nie polecam");
-//        System.out.println("Received result: " + result);
-//    }
-//}
+/*
+ TODO do usunięcia po zakończeniu implementacji modelu - kod dla testów
+public class JorgXApplication {
+
+    public static void main(String[] args) {
+        ModelAPIConnector modelClient = new ModelAPIConnector("http://localhost:5000/predict");
+        String result = modelClient.makeRequest("Nie polecam");
+        System.out.println("Received result: " + result);
+    }
+}
+*/
--- src/main/java/pl/jorgX/controller/UserController.java
+++ src/main/java/pl/jorgX/controller/UserController.java
@@ -2,6 +2,8 @@ package pl.jorgX.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import pl.jorgX.database.user.*;
 import pl.jorgX.services.UserService;
@@ -21,6 +23,7 @@ public class UserController {
 
     private final UserService userService;
     private final UserMapper userMapper;
+    private final UserRepository userRepository;
 
     @PostMapping
     public UserInfoDTO createUser(@RequestBody @Valid UserCreateDTO user) {
@@ -60,5 +63,17 @@ public class UserController {
     public void deleteUser(@PathVariable UUID id) {
         log.debug("Deleting user {}", id);
         userService.delete(id);
+    }
+
+    @GetMapping("/currentUser")
+    public UserDAO getCurrentUser() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+        return userRepository.findByEmail(username).orElseThrow(() -> new RuntimeException("User not found"));
     }
 }
